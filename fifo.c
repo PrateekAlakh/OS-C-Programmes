c7bc5e9148ed874e76bc439e1d04bc5b4876c762
PrateekAlakh <alakhprateek321@gmail.com>
2017-06-09 21:26:11 +0530

    first commit

---
+++ fifo.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n,buf[4]={0},a[20],hit=0,miss=0;
+    printf("Enter limit : ");
+    scanf("%d",&n);
+    printf("Enter values :\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            if(a[i]==buf[j])
+            {
+                hit++;
+                break;
+            }
+        }
+        if(j==4)
+        {
+            miss++;
+            for(k=3;k>0;k--)
+                buf[k]=buf[k-1];
+            buf[0]=a[i];
+        }
+    }
+    printf("Hit = %d\nMiss = %d",hit,miss);
+    return 0;
+}
---
+++ lala.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void check(int n)
+{
+    int i,j,max[n][n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("he;l\n");
+        }
+    }
+
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    check(n);
+}
